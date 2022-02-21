2dcfc87b3d1be34a427d739f9f8ecc7eaa02e69e
linxbest <2580070017@qq.com>
2022-02-21 23:15:04 +0800

    首次提交

---
+++ js/content-script.js
@@ -0,0 +1,47 @@
+﻿/** 
+ * 注：本模块目前暂时针对 
+ * 	   https://www.fashionnova.com/collections/new 
+ *     进行测试数据采集，暂时不支持其他网站后续会慢慢补充和加强通用能力
+ */
+(function() {
+	console.log('content script start!');
+	createOnRequest();// 创建监听消息事件
+})();
+
+function getGoods() {
+	let nodeList = document.querySelectorAll('.collection-list__product-tile');
+	let goods = [];
+	for (let nodeItem of nodeList) {
+		goods.push({
+			cover: getItemCover(nodeItem),
+			title: getItemTitle(nodeItem),
+			price: getItemPriceNum(nodeItem),
+		})
+	}
+	return goods.sort((prev, next) => prev.price > next.price);
+}
+
+function getItemTitle(nodeItem) {
+	const $item = nodeItem.querySelector('.product-tile__product-title')
+	return $item? $item.innerText : '';
+}
+
+function getItemCover(nodeItem) {
+	const $img = nodeItem.querySelector('.product-tile__image')
+	return $img? $img.getAttribute('src') : '';
+}
+
+function getItemPriceNum(nodeItem) {
+	const $money = nodeItem.querySelector('.money');
+	const money = $money? $money.innerText : '';
+	return window.parseFloat(money.replace('$', '')) || 0
+}
+
+function createOnRequest() {
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			// 收到消息目前暂时不做分析处理，统一执行获取商品信息
+			let goodsList = getGoods();
+			sendResponse(goodsList)
+	  });	  
+}
---
+++ js/popup.js
@@ -0,0 +1,48 @@
+﻿/** 
+ *	本模块尚未完成功能：
+ *	1.全屏显示
+ *	2.页面截图
+ *	3.popup.html样式及内容填充
+ */
+
+$(function() {
+	console.log('popupjs start!')
+	fetchGoodsInfo(); // 向content script发送请求获取商品信息
+	getScreenshot(); // 获取页面截图（暂未完成）
+});
+
+function getScreenshot() {
+	// alert('执行截图')
+	// chrome.desktopCapture.chooseDesktopMedia(
+	//     "screen",
+	//     '',
+	//     function (res) {
+	//         console.log(res)
+		
+	//     },
+	//   )
+	chrome.tabs.captureVisibleTab({
+        format: "png"
+    }, function(capture) {
+		console.log("截图结果",capture)
+        // alert('截图数据'+capture)
+        
+    })
+}
+
+function fetchGoodsInfo() {
+	chrome.tabs.query(
+		{active: true, currentWindow: true}, 
+		function(tabs) {
+			chrome.tabs.sendMessage(
+				tabs[0].id, 
+			    { name: "getGoods" }, 
+			    function(response) {
+					let list = response;
+					let total = list.length;
+					console.log('商品总数', total)
+					console.log('最高价格商品', list[0])
+					console.log('最低价格商品', list[total-1])
+				});
+		});
+}
